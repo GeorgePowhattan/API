d2509f220fce9567d4fb683924639c8798f1e10f
Ondřej Martínek <46747310+GeorgePowhattan@users.noreply.github.com>
2019-10-22 16:13:24 +0200

    Create functions.py

---
+++ Ebay/functions.py
@@ -0,0 +1,32 @@
+def misspelling_checker(item):
+    res = []
+    
+    return result
+    
+    
+def rotation(item):
+    res = []
+    for i in range(0, len(item)-1):
+        res.append(item[0:i] + item[i:i+2][::-1] + item[i+2:])
+    return res
+    
+    
+def missing(item):
+    res = []
+    for i in range(0, len(a)-1):
+        res.append(item.replace(item[i],''))
+    return res
+    
+    
+def adding(item):
+    res=[]
+    for i in range(0, len(a)-1):
+        res.append(item.replace(item[i],item[i]*2))
+    return res
+    
+    
+def fat_fingers(item)
+    res=[]
+    for i in range(0, len(a)-1):
+        res.append(item.replace(item[i],item[i]*2))
+    return res
