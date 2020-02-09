b84fe8f2a686566aea8ab87f9fc1e26eebdb15da
GeorgePowhattan <martinek.ondrej@gmail.com>
2020-02-09 21:35:34 +0100

    update

--- API_RESTful/api.py
+++ API_RESTful/api.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask import abort, make_response   # for error handling
 from flask import request               # for POST method
-
+from flask import url_for               # to swap the task id for an URI
 
 app = Flask(__name__)
 
@@ -20,9 +20,20 @@ tasks = [
     }
 ]
   
+def make_public_task(task):    # to fetch the uri, not task 'id' - which needs to be entered as an argument into todo/api/v1.0/tasks/<int:task_id> querry
+    new_task = {}
+    for field in task:          # id, title, description, ...
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
@@ -31,9 +42,11 @@ def get_task(task_id):
         abort(404)
     return jsonify({'task': task})
 
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
 def create_task():
@@ -53,10 +66,3 @@ def create_task():
 if __name__ == "__main__":
     app.run(debug = True)
 
-
-
-    '''
-    for task in tasks:
-        if task['id'] == task_id:
-            task = task['id']
-    '''
